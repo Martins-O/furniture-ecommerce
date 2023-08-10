e6d3028364b4a545b910fcd0e2b2552a66f7df92
martins-o <jojololamartins686@gmail.com>
2023-08-10 09:42:39 +0100

    Continuos pushing

---
+++ src/main/java/furniture/ecormmerce/furnitureapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package furniture.ecormmerce.furnitureapi.exception;
+
+import furniture.ecormmerce.furnitureapi.data.dto.response.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static furniture.ecormmerce.furnitureapi.common.Messages.EMAIL_ALREADY_EXIST;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public ApiResponse handleUserNotFoundException(HttpServletRequest request,
+	                                        HttpServletResponse response){
+		return ApiResponse.builder()
+				.message ("User not found")
+				.statusCode(HttpServletResponse.SC_NOT_FOUND)
+				.isSuccessful (false)
+				.build();
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public ApiResponse handleUserAlreadyExistException(HttpServletRequest request,
+	                                        HttpServletResponse response){
+		return ApiResponse.builder()
+				.message (EMAIL_ALREADY_EXIST)
+				.statusCode(HttpServletResponse.SC_FORBIDDEN)
+				.isSuccessful (false)
+				.build();
+	}
+	
+	
+}
