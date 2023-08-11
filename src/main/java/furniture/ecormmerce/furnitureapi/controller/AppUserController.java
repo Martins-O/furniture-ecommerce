e5fe7d11145946905ac9a810b3c5301c761a536e
martins-o <jojololamartins686@gmail.com>
2023-08-11 15:40:49 +0100

    Corrections

--- src/main/java/furniture/ecormmerce/furnitureapi/controller/AppUserController.java
+++ src/main/java/furniture/ecormmerce/furnitureapi/controller/AppUserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("api/v1/users/")
+@RequestMapping("/api/v1/users/")
 public class AppUserController {
 	
 	private final AppUserService service;
