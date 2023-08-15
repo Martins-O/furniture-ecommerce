729b481f129ff13e2f5ad7d421ab225f1eae415c
martins-o <jojololamartins686@gmail.com>
2023-08-15 09:45:03 +0100

    Update of method

--- src/main/java/furniture/ecormmerce/furnitureapi/FurnitureApiApplication.java
+++ src/main/java/furniture/ecormmerce/furnitureapi/FurnitureApiApplication.java
@@ -1,9 +1,23 @@
 package furniture.ecormmerce.furnitureapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Furniture API",
+				version = "v1",
+				description = "This app provides REST APIs for Furniture",
+				contact = @Contact(
+						name = "Furniture Support",
+						email = "jojololamartins686@gmail.com"
+				)
+		)
+)
 public class FurnitureApiApplication {
 	
 	public static void main(String[] args) {
